96e612dcafdc1ab5077f94aa06a68caeb61fac2b
jcomperatore <125047366+jcomperatore@users.noreply.github.com>
2023-02-10 17:57:39 +0100

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,7 +1,7 @@
 from graph import Graph, graph_from_file
 
 
-data_path = "input/"
+data_path = "/home/onyxia/ProjetProg-Ensae/input/"
 file_name = "network.01.in"
 
 g = graph_from_file(data_path + file_name)
